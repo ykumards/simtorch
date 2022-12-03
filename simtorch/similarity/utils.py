7f488cabdd99be943987e26aaf6d45960a5ca3bf
ykumards <ykumar@nyu.edu>
2022-12-03 21:52:55 +0200

    add remove negative eigen

--- simtorch/similarity/utils.py
+++ simtorch/similarity/utils.py
@@ -2,7 +2,7 @@ import torch
 
 
 def remove_negative_eigenvalues(A):
-    L, V = torch.linalg.eig(A)
-    L[torch.view_as_real(L)[:, 0] < 0] = 0
+    L, V = torch.linalg.eigh(A)
+    L[L < 0] = 0
 
-    return torch.view_as_real(V @ torch.diag_embed(L) @ torch.linalg.inv(V))[:, :, 0]
+    return V @ torch.diag_embed(L) @ V.T
