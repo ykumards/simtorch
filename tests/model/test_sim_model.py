bfc91459240e2c3a4cd98637e9e8aa3bee2855c9
ykumards <ykumar@nyu.edu>
2022-12-03 11:57:17 +0200

    add new test

--- tests/model/test_sim_model.py
+++ tests/model/test_sim_model.py
@@ -5,6 +5,7 @@ from simtorch.model.sim_model import SimilarityModel
 
 class DummyModel(torch.nn.Module):
     def __init__(self):
+        super().__init__()
         self.layer1 = torch.nn.Linear(2, 4)
         self.act1 = torch.nn.ReLU()
         self.layer2 = torch.nn.Linear(4, 1)
@@ -14,14 +15,11 @@ class DummyModel(torch.nn.Module):
 
 
 class TestSimModel:
-    def test_init():
+    def test_init(self):
         dummy_model = DummyModel()
 
         sim_model = SimilarityModel(
-            model=dummy_model,
-            model_name="test_model",
-            layers_to_include=["linear"],
-            device="cpu"
+            model=dummy_model, model_name="test_model", layers_to_include=["linear"], device="cpu"
         )
 
         assert isinstance(sim_model, SimilarityModel)
